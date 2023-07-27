27004b01e086b62402d6514173c5954a44d92072
Anand Abhishek <84555231+aanand03@users.noreply.github.com>
2023-07-27 16:52:23 +0530

    Add files via upload

---
+++ src/components/Hero.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from "styled-components";
+import { other_images } from '../utils/images';
+
+const Hero = () => {
+  return (
+    <HeroWrapper className = "bg-black">
+      <div className='container h-100 flex'>
+        <div className='hero-content'>
+          <h1>Keep Moving Up</h1>
+          <p>Learn the skills you need to take the next step — and every step after. Log in now to save on courses. Sale ends today.</p>
+        </div>
+      </div>
+    </HeroWrapper>
+  )
+}
+
+const HeroWrapper = styled.div`
+  background: url(${other_images.hero_img}) center/cover no-repeat;
+  height: 300px;
+
+  .container{
+    .hero-content{
+      background-color: var(--clr-white);
+      opacity: 0.6;
+      max-width: 400px;
+      width: 100%;
+      margin-left: 0;
+      padding: 20px;
+
+      h1{
+        font-size: 32px;
+        margin-bottom: 5px;
+        white-space: nowrap;
+      }
+      p{
+        font-size: 15px;
+      }
+    }
+  }
+`;
+
+export default Hero
